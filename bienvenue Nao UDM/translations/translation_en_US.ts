425261dcc849a112da1af818e79fb2e731123a5f
Cyril Romero <cyril.romero@etu.unilim.fr>
2024-05-07 09:29:21 +0200

    fichier script bienvenue

---
+++ bienvenue Nao UDM/translations/translation_en_US.ts
@@ -0,0 +1,101 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello wilrd</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello wilrd</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/campus/behavior_layer1/texte/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Nous avons actuellement deux campus entièrement opérationnels, l'un à Roches Brunes, Rose Hill, et l'autre à Beau Plan, Pamplemousses. Vous aurez l'occasion de visiter notre campus, ici à Rose Hill aujourd'hui, et d'en apprendre davantage sur nos installations et nos programmes.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Nous avons actuellement deux campus entièrement opérationnels, l'un à Roches Brunes, Rose Hill, et l'autre à Beau Plan, Pamplemousses. Vous aurez l'occasion de visiter notre campus, ici à Rose Hill aujourd'hui, et d'en apprendre davantage sur nos installations et nos programmes.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/missions et valeur/behavior_layer1/texte/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Notre mission est de promouvoir l'excellence dans l'éducation de qualité, la recherche et l'innovation sur nos trois sites, à savoir, le campus de Rose Hill, le campus de Stanley et le campus de Swami Dayanand. Nous sommes engagés à fournir une éducation de qualité à nos étudiants et à contribuer au développement durable de notre région.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Notre mission est de promouvoir l'excellence dans l'éducation de qualité, la recherche et l'innovation sur nos trois sites, à savoir, le campus de Rose Hill, le campus de Stanley et le campus de Swami Dayanand. Nous sommes engagés à fournir une éducation de qualité à nos étudiants et à contribuer au développement durable de notre région.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/partenariat/behavior_layer1/texte/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Nous avons un partenariat étroit avec l'Université de Limoges, et de nombreux de nos diplômes sont des diplômes doubles avec l'Université de Limoges. Ce partenariat nous permet d'offrir des programmes d'études de haute qualité et de renforcer notre position en tant qu'université de premier plan dans la région.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Nous avons un partenariat étroit avec l'Université de Limoges, et de nombreux de nos diplômes sont des diplômes doubles avec l'Université de Limoges. Ce partenariat nous permet d'offrir des programmes d'études de haute qualité et de renforcer notre position en tant qu'université de premier plan dans la région.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/présentation université/behavior_layer1/texte pres UDM/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Avant de commencer cette visite, laissez-moi vous présenter notre université, l'Université des Mascareignes a été créée en 2012 et est la plus jeune université publique de Maurice, sous l'égide du ministère de l'Éducation, de l'Enseignement supérieur, de la Science et de la Technologie. Nous proposons des programmes de premier cycle et de troisième cycle en développement durable et en ingénierie, en commerce et en gestion, ainsi qu'en technologie de l'information et de la communication. Nous proposons également des programmes de recherche au niveau doctoral.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Avant de commencer cette visite, laissez-moi vous présenter notre université, l'Université des Mascareignes a été créée en 2012 et est la plus jeune université publique de Maurice, sous l'égide du ministère de l'Éducation, de l'Enseignement supérieur, de la Science et de la Technologie. Nous proposons des programmes de premier cycle et de troisième cycle en développement durable et en ingénierie, en commerce et en gestion, ainsi qu'en technologie de l'information et de la communication. Nous proposons également des programmes de recherche au niveau doctoral.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/salutation/behavior_layer1/texte de salutation/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Bonjour et bienvenue à l'Université des Mascareignes ! Je suis ravi de vous accueillir ici et j'espère que vous apprécierez votre visite.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Bonjour et bienvenue à l'Université des Mascareignes ! Je suis ravi de vous accueillir ici et j'espère que vous apprécierez votre visite.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/test/behavior_layer1/texte/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Hello world</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello world</translation>
+        </message>
+    </context>
+</TS>
